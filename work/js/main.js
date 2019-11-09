c37ccbaa16b37b3c6faf8b28c3ae872c84fd31be
Gia Do <gia.dohoang@gmail.com>
2019-11-09 14:18:03 -0500

    Update main.js

--- work/js/main.js
+++ work/js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var localConnection;
 var remoteConnection;
@@ -6,16 +6,15 @@ var sendChannel;
 var receiveChannel;
 var pcConstraint;
 var dataConstraint;
-var dataChannelSend = document.querySelector('textarea#dataChannelSend');
-var dataChannelReceive = document.querySelector('textarea#dataChannelReceive');
-var startButton = document.querySelector('button#startButton');
-var sendButton = document.querySelector('button#sendButton');
-var closeButton = document.querySelector('button#closeButton');
-
+var dataChannelSend = document.querySelector("textarea#dataChannelSend");
+var dataChannelReceive = document.querySelector("textarea#dataChannelReceive");
+var startButton = document.querySelector("button#startButton");
+var sendButton = document.querySelector("button#sendButton");
+var closeButton = document.querySelector("button#closeButton");
+var inputBox = document.querySelector("dataChannelSend");
 startButton.onclick = createConnection;
 sendButton.onclick = sendData;
 closeButton.onclick = closeDataChannels;
-
 function enableStartButton() {
   startButton.disabled = false;
 }
@@ -25,21 +24,25 @@ function disableSendButton() {
 }
 
 function createConnection() {
-  dataChannelSend.placeholder = '';
+  dataChannelSend.placeholder = "";
   var servers = null;
   pcConstraint = null;
   dataConstraint = null;
-  trace('Using SCTP based data channels');
+  trace("Using SCTP based data channels");
   // For SCTP, reliable and ordered delivery is true by default.
   // Add localConnection to global scope to make it visible
   // from the browser console.
-  window.localConnection = localConnection =
-      new RTCPeerConnection(servers, pcConstraint);
-  trace('Created local peer connection object localConnection');
+  window.localConnection = localConnection = new RTCPeerConnection(
+    servers,
+    pcConstraint
+  );
+  trace("Created local peer connection object localConnection");
 
-  sendChannel = localConnection.createDataChannel('sendDataChannel',
-      dataConstraint);
-  trace('Created send data channel');
+  sendChannel = localConnection.createDataChannel(
+    "sendDataChannel",
+    dataConstraint
+  );
+  trace("Created send data channel");
 
   localConnection.onicecandidate = iceCallback1;
   sendChannel.onopen = onSendChannelStateChange;
@@ -47,47 +50,48 @@ function createConnection() {
 
   // Add remoteConnection to global scope to make it visible
   // from the browser console.
-  window.remoteConnection = remoteConnection =
-      new RTCPeerConnection(servers, pcConstraint);
-  trace('Created remote peer connection object remoteConnection');
+  window.remoteConnection = remoteConnection = new RTCPeerConnection(
+    servers,
+    pcConstraint
+  );
+  trace("Created remote peer connection object remoteConnection");
 
   remoteConnection.onicecandidate = iceCallback2;
   remoteConnection.ondatachannel = receiveChannelCallback;
 
-  localConnection.createOffer().then(
-    gotDescription1,
-    onCreateSessionDescriptionError
-  );
+  localConnection
+    .createOffer()
+    .then(gotDescription1, onCreateSessionDescriptionError);
   startButton.disabled = true;
   closeButton.disabled = false;
 }
 
 function onCreateSessionDescriptionError(error) {
-  trace('Failed to create session description: ' + error.toString());
+  trace("Failed to create session description: " + error.toString());
 }
 
 function sendData() {
   var data = dataChannelSend.value;
   sendChannel.send(data);
-  trace('Sent Data: ' + data);
+  trace("Sent Data: " + data);
 }
 
 function closeDataChannels() {
-  trace('Closing data channels');
+  trace("Closing data channels");
   sendChannel.close();
-  trace('Closed data channel with label: ' + sendChannel.label);
+  trace("Closed data channel with label: " + sendChannel.label);
   receiveChannel.close();
-  trace('Closed data channel with label: ' + receiveChannel.label);
+  trace("Closed data channel with label: " + receiveChannel.label);
   localConnection.close();
   remoteConnection.close();
   localConnection = null;
   remoteConnection = null;
-  trace('Closed peer connections');
+  trace("Closed peer connections");
   startButton.disabled = false;
   sendButton.disabled = true;
   closeButton.disabled = true;
-  dataChannelSend.value = '';
-  dataChannelReceive.value = '';
+  dataChannelSend.value = "";
+  dataChannelReceive.value = "";
   dataChannelSend.disabled = true;
   disableSendButton();
   enableStartButton();
@@ -95,56 +99,49 @@ function closeDataChannels() {
 
 function gotDescription1(desc) {
   localConnection.setLocalDescription(desc);
-  trace('Offer from localConnection \n' + desc.sdp);
+  trace("Offer from localConnection \n" + desc.sdp);
   remoteConnection.setRemoteDescription(desc);
-  remoteConnection.createAnswer().then(
-    gotDescription2,
-    onCreateSessionDescriptionError
-  );
+  remoteConnection
+    .createAnswer()
+    .then(gotDescription2, onCreateSessionDescriptionError);
 }
 
 function gotDescription2(desc) {
   remoteConnection.setLocalDescription(desc);
-  trace('Answer from remoteConnection \n' + desc.sdp);
+  trace("Answer from remoteConnection \n" + desc.sdp);
   localConnection.setRemoteDescription(desc);
 }
 
 function iceCallback1(event) {
-  trace('local ice callback');
+  trace("local ice callback");
   if (event.candidate) {
-    remoteConnection.addIceCandidate(
-      event.candidate
-    ).then(
-      onAddIceCandidateSuccess,
-      onAddIceCandidateError
-    );
-    trace('Local ICE candidate: \n' + event.candidate.candidate);
+    remoteConnection
+      .addIceCandidate(event.candidate)
+      .then(onAddIceCandidateSuccess, onAddIceCandidateError);
+    trace("Local ICE candidate: \n" + event.candidate.candidate);
   }
 }
 
 function iceCallback2(event) {
-  trace('remote ice callback');
+  trace("remote ice callback");
   if (event.candidate) {
-    localConnection.addIceCandidate(
-      event.candidate
-    ).then(
-      onAddIceCandidateSuccess,
-      onAddIceCandidateError
-    );
-    trace('Remote ICE candidate: \n ' + event.candidate.candidate);
+    localConnection
+      .addIceCandidate(event.candidate)
+      .then(onAddIceCandidateSuccess, onAddIceCandidateError);
+    trace("Remote ICE candidate: \n " + event.candidate.candidate);
   }
 }
 
 function onAddIceCandidateSuccess() {
-  trace('AddIceCandidate success.');
+  trace("AddIceCandidate success.");
 }
 
 function onAddIceCandidateError(error) {
-  trace('Failed to add Ice Candidate: ' + error.toString());
+  trace("Failed to add Ice Candidate: " + error.toString());
 }
 
 function receiveChannelCallback(event) {
-  trace('Receive Channel Callback');
+  trace("Receive Channel Callback");
   receiveChannel = event.channel;
   receiveChannel.onmessage = onReceiveMessageCallback;
   receiveChannel.onopen = onReceiveChannelStateChange;
@@ -152,14 +149,14 @@ function receiveChannelCallback(event) {
 }
 
 function onReceiveMessageCallback(event) {
-  trace('Received Message');
+  trace("Received Message");
   dataChannelReceive.value = event.data;
 }
 
 function onSendChannelStateChange() {
   var readyState = sendChannel.readyState;
-  trace('Send channel state is: ' + readyState);
-  if (readyState === 'open') {
+  trace("Send channel state is: " + readyState);
+  if (readyState === "open") {
     dataChannelSend.disabled = false;
     dataChannelSend.focus();
     sendButton.disabled = false;
@@ -173,16 +170,16 @@ function onSendChannelStateChange() {
 
 function onReceiveChannelStateChange() {
   var readyState = receiveChannel.readyState;
-  trace('Receive channel state is: ' + readyState);
+  trace("Receive channel state is: " + readyState);
 }
 
 function trace(text) {
-  if (text[text.length - 1] === '\n') {
+  if (text[text.length - 1] === "\n") {
     text = text.substring(0, text.length - 1);
   }
   if (window.performance) {
     var now = (window.performance.now() / 1000).toFixed(3);
-    console.log(now + ': ' + text);
+    console.log(now + ": " + text);
   } else {
     console.log(text);
   }
